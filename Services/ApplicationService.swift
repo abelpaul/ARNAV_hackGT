78df81d9390e8fbb5605186f8f6e2c53511ab2c8
Abel <abel.paul.george@gmail.com>
2017-10-13 23:28:31 -0400

    LocationServiceFiles

---
+++ Services/ApplicationService.swift
@@ -0,0 +1,16 @@
+//
+//  Application Service.swift
+//  ARNAV_hackGT
+//
+//  Created by Abel George on 10/13/17.
+//  Copyright © 2017 Abel George. All rights reserved.
+//
+
+
+import Foundation
+
+struct ApplicationServices {
+    var navService: NavigationService
+    var locationService: LocationService
+}
+
